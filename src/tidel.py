2aa6897b8e9c98c49c14accca48c9f80cfe7836e
mtx <mtx@storage1.(none)>
2015-12-02 15:54:37 +0800

    12-2-2

--- src/tidel.py
+++ src/tidel.py
@@ -10,23 +10,24 @@ class Tidels():
         data=f.read()
         f.close()
         data=struct.unpack('1073741824f',data)
-        data=np.array(data,dtype=np.float)
+        data=np.array(data,dtype=np.float16)
         data=data.reshape((1024,1024,1024),order='F')
         return data
 N=1024
-H=1.2/1024.
-kn=np.pi/H
+L=1.2
+H=L/1024.
+kf=2*np.pi/L
 data=Tidels.LoadData(filename='/home/mtx/data/tidels/0.000den00.bin')
 delta_k=np.fft.fftn(data)
 del data
 x=np.arange(1024)
-for i in np.arange(1,1024/2+1,dtype=float):
+for i in np.arange(1,1024/2+1):
     x[1024-i]=x[i]
 window_k=np.sinc(np.pi/N*x[:,None,None])*np.sinc(np.pi/N*x[None,:,None])*np.sinc(np.pi/N*x[None,None,:])
 Pk=np.abs(delta_k/window_k)**2
 del window_k
 #########################################################################
-k=(x[:,None,None]**2+x[None,:,None]**2+x[None,None,:]**2)**(1./2.)
+k=np.log10(2*np.pi/L*((x[:,None,None]**2+x[None,:,None]**2+x[None,None,:]**2)**(1./2.)))
 kmax=k.max()
 kmin=k.min()
 x=np.linspace(kmin,kmax,20)
@@ -35,8 +36,9 @@ P=[]
 for i in x:
     P.append(Pk[((i-dx/2.)<=k)*(k<(i+dx/2.))].sum()/float(len(Pk[((i-dx/2.)<=k)*(k<(i+dx/2.))])))
 P=1.2**3./(1024.**6.)*np.array(P)
-plt.plot(x,np.log10(P))
-plt.savefig('./ps.png')
+np.savetxt('PS_data',np.c_[x,np.log10(P)])
+#plt.plot(x,np.log10(P),'-o')
+#plt.savefig('./ps.png')
 
 
 
