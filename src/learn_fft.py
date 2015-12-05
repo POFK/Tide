a81a81db7e777c712ae03662f312a4ef1eae7ec0
mtianxiang <maotianxiang@bao.ac.cn>
2015-12-05 14:25:49 +0800

    1205 1425

--- src/learn_fft.py
+++ src/learn_fft.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
 # coding=utf-8
 import numpy as np
-import 
+import matplotlib.pyplot as plt 
+N=2000
+L=3
+a=np.linspace(0,L,N)
+x=a
+b=np.sin(156.25*x)  + 2*np.sin(234.375*x)
+#freq=np.abs(freq)
+b=b
+freq=2*np.pi/L*np.fft.fftfreq(len(b),1./len(b))
+fy=np.fft.fft(b)/len(b)
+print fy[0]
+P=np.abs(fy)**2
+plt.plot(freq,P,'-o')
+plt.show()
