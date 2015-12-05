4697e342c282e060f586db74c242b89511917629
POFK <maotianxiang@bao.ac.cn>
2015-12-05 16:08:07 +0800

    test

--- push_to_git.sh
+++ push_to_git.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #git clone https://github.com/POFK/test.git
 git add .
-#git commit -m 'add src of pdflatex to write tex file to pdf'
-git commit -a 
+git commit -m 'test'
+
+#`data`
+#git commit -a 
 git push origin master
