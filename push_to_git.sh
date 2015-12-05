52910bea4226e530eb1d17143662db008db9ebce
POFK <maotianxiang@bao.ac.cn>
2015-12-05 16:25:35 +0800

    2015/12/05_16:25:35

--- push_to_git.sh
+++ push_to_git.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #git clone https://github.com/POFK/test.git
+com=`date '+%Y/%m/%d_%H:%M:%S'`
 git add .
-git commit -m date `'+%Y/%m/%d %H:%M:%S'`
+git commit -m $com
 #git commit -a 
 git push origin master
