b41374c3910efa1b746334b076b64b1e89ca8e89
mtianxiang <maotianxiang@bao.ac.cn>
2015-12-09 10:57:32 +0800

    2015/12/09_10:57:32

--- src/test.py
+++ src/test.py
@@ -4,7 +4,7 @@ import numpy as np
 import h5py
 ###############load log(delta x)#######################
 f = h5py.File('0.000den00_smoothg.hdf5')
-data = f['data'].value
+data = np.array(f['data'].value,dtype=np.float16)
 print np.dtype(data)
 f.close()
 ############### partial x #############################
