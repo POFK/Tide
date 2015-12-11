0d302ee3eff65075fdccf4cb0631b61d34f2733a
POFK <maotianxiang@bao.ac.cn>
2015-12-11 17:08:16 +0800

    2015/12/11_17:08:16

---
+++ src/delta_w.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding=utf-8
+import numpy as np
+import h5py
+###############load log(delta x)#######################
+f = h5py.File('/home/mtx/data/tide/outdata/0.000den00_smoothg.hdf5')
+data =np.array( f['data'].value,dtype=np.float16)
+print data.shape
+f.close()
+############### partial x #############################
+N=1024
+L=1.2*10**3
+nx=np.fft.fftfreq(N,1./N)
+Kx=2*np.pi/L*nx
+fk=np.fft.fftn(data)
+del data
+S=np.ones_like(fk)
+for i in range(N):
+    S[i,:,:]=fk[i,:,:]*Kx[i]*1j
+
+dtype=np.dtype([('partialX_delta','f4')])
+result=np.array(np.fft.ifftn(S).real,dtype=dtype)
+f=h5py.File('/home/mtx/data/tide/outdata/0.000den00_wdensgx.hdf5',mode='w')
+f.create_dataset(name='data',data=S)
+f.close()
