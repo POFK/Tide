1d76bcc641b9f02c2cb9fcb060aeb8298f98f618
mtianxiang <maotianxiang@bao.ac.cn>
2015-12-13 11:44:37 +0800

    2015/12/13_11:44:37

--- src/delta_w.py
+++ src/delta_w.py
@@ -2,26 +2,31 @@
 # coding=utf-8
 import numpy as np
 import h5py
-import TIDES as ti
+from TIDES import *
 ###############load log(delta x)#######################
 f = h5py.File('/home/mtx/data/tide/outdata/0.000den00_smoothg.hdf5')
 data =np.array( f['data'].value,dtype=np.float16)
 print data.shape
 f.close()
-############### partial x #############################
-alpha,beta=ti.Tide.Get_Alpha_Beta()
+############### delta_g^x #############################
+wk=Tide.Get_wk()
 N=1024
 L=1.2*10**3
 nx=np.fft.fftfreq(N,1./N)
 Kx=2*np.pi/L*nx
-fk=np.fft.fftn(data)
+zero=np.zeros_like(Kx)
+K=np.sqrt(Kx[:,None,None]**2+Kx[None,:,None]**2+Kx[None,None,:]**2)
+delta_gk=np.fft.fftn(data)
 del data
-S=np.ones_like(fk)
-for i in range(N):
-    S[i,:,:]=fk[i,:,:]*Kx[i]*1j
-
+delta_gk_wx=delta_gk*wk(K)*1j*(Kx[:,None,None]+zero[None,:,None]+zero[None,None,:])
+del delta_gk
+del K
 dtype=np.dtype([('partialX_delta','f4')])
-result=np.array(np.fft.ifftn(S).real,dtype=dtype)
+result=np.array(np.abs(np.fft.ifftn(delta_gk_wx)),dtype=dtype)
+del delta_gk_wx
 f=h5py.File('/home/mtx/data/tide/outdata/0.000den00_wdensgx.hdf5',mode='w')
 f.create_dataset(name='data',data=S)
 f.close()
+g=open('./home/mtx/data/tide/outdata/0.000den00_wdensgx.bin','wb')
+g.write(result.reshape(-1))
+g.close()
