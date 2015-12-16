d9d103556924e08e4b9311db0e92f6c8a1a7b27e
POFK <maotianxiang@bao.ac.cn>
2015-12-16 09:49:28 +0800

    2015/12/16_09:49:28

---
+++ mpi_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+from mpi4py import MPI
+import numpy as np
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+if rank==0:
+    data=np.arange(size*2)
+    data=data.reshape(size,-1)
+else :
+    data=[]
+a=comm.scatter(data,root=0)
+
+print rank,a
+combine_data = comm.gather(a,root=0)
+if rank==0:
+    combine_data=np.array(combine_data).reshape(-1)
+    print combine_data
--- src/result.py
+++ src/result.py
@@ -7,23 +7,27 @@ from mpi4py import MPI
 '''get kappa(kv,kp),b,w, and get kappa(x)'''
 N=1024
 L=1.2*10**3
-#f=h5py.File('/home/mtx/data/tide/outdata/0.000den00_wkappa3d_x.hdf5')
-#kappa3dx=np.array(f['data'].value,dtype=np.float16)
-#f.close()
-################################################################################
+#################################################################################
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
-################################################################################
-bins=10
-x=np.fft.fftfreq(N,1./N)
-z=np.zeros_like(x)
-kv = ((x**2)[:, None, None] +
-      (x**2)[None, :, None] +
-      (z**2)[None, None, :])**(1. / 2.)
-kp = ((z**2)[:, None, None] +
-      (z**2)[None, :, None] +
-      (x**2)[None, None, :])**(1. / 2.)
-kn_max = 512
-kn_min = 0
+#################################################################################
+#f=h5py.File('/home/mtx/data/tide/outdata/0.000den00_wkappa3d_x.hdf5')
+#kappa3dx=np.array(f['data'].value,dtype=np.float16)
+#f.close()
+#################################################################################
+if rank == 0 :
+#    f=h5py.File('/home/mtx/data/tide/outdata/0.000den00_wkappa3d_x.hdf5')
+#    kappa3dx=np.array(f['data'].value,dtype=np.float16)
+#    f.close()
+
+    bins=10
+    x=np.fft.fftfreq(N,1./N)
+    z=np.zeros_like(x)
+    kv = ((x**2)[:, None, None] +(x**2)[None, :, None] +(z**2)[None, None, :])**(1. / 2.)
+    kp = ((z**2)[:, None, None] +(z**2)[None, :, None] +(x**2)[None, None, :])**(1. / 2.)
+    kv=kv.reshape(-1)
+    kp=kp.reshape(-1)
+    kn_max = 512
+    kn_min = 0
 
