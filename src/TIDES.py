f9e588def9d4c78b55f41bcd79272f51827fd7e9
POFK <maotianxiang@bao.ac.cn>
2015-12-19 20:42:01 +0800

    2015/12/19_20:42:01

--- src/TIDES.py
+++ src/TIDES.py
@@ -16,12 +16,28 @@ class Tide():
     @classmethod
     def LoadData(self, filename='/home/mtx/data/tide/0.000den00.bin'):
         '''read bin data'''
+        print 'Loading data.................'
         f = open(filename, 'rb')
         data = f.read()
         f.close()
         data = struct.unpack('1073741824f', data)
         data = np.array(data, dtype=np.float16)
         data = data.reshape((1024, 1024, 1024), order='F')
+        return data
+
+    @classmethod
+    def SaveDataHdf5(self,data,filename):
+        print 'Save data....................'
+        dtype=np.dtype([('data','f4')])
+        f=h5py.File(filename,mode='w')
+        f.create_dataset(name='data',data=data)
+        f.close()
+
+    @classmethod
+    def LoadDataOfhdf5(self,filename):
+        f=h5py.File(filename)
+        data=np.array(f['data'].value,dtype=np.float)
+        f.close()
         return data
 
     @classmethod
@@ -91,8 +107,11 @@ class Tide():
 #           return (fk(k)/Pk(k))**0.5/k
             return (fk(k)/Pk(k)/Q)**0.5/k
         return wk
+
     @classmethod
     def Smooth(self,data,sigma=1.25,log=True):
+
+        print 'Smoothing....................'
         Kf=2*np.pi/(1.2*10**3)
         x = np.fft.fftfreq(N,1./N)  # x: 0,1,2,...,512,-511,...,-2,-1
         delta_k=np.fft.fftn(data)
@@ -100,19 +119,63 @@ class Tide():
         window_k = np.sinc( 1./N* x[:,None,None]) * np.sinc( 1./N * x[None,:,None]) * np.sinc( 1./N * x[None,None,:])
         smoothed_k=(delta_k*np.exp(-0.5*(Kf*Kf)*k*k*sigma**2))/window_k
         smoothed_x=np.fft.ifftn(smoothed_k)
+        print 'Smoothing..................OK'
         if log==True:
             return np.log10(np.abs(smoothed_x))
         if log==False:
             return smoothed_x.real
+
     @classmethod
-    def SaveDataHdf5(self,data,filename):
-        dtype=np.dtype([('data','f4')])
-        f=h5py.File(filename,mode='w')
-        f.create_dataset(name='data',data=data)
-        f.close()
-    @clasmethod
-    def LoadDataOfhdf5(self,filename):
-        f=h5py.File(filename)
-        data=np.array(f['data'].value,dtype=np.float)
-        f.close()
-        return data
+    def DeltagW(self,data):
+        '''return (deltagx,deltagy)'''
+        print 'calculating deltaW...........'
+
+        wk=Tide.Get_wk()
+        nx=np.fft.fftfreq(N,1./N)
+        Kx=2*np.pi/L*nx
+        zero=np.zeros_like(Kx)
+        K=np.sqrt(Kx[:,None,None]**2+Kx[None,:,None]**2+Kx[None,None,:]**2)
+        K[0,0,0]=10**(-4)
+        delta_gk=np.fft.fftn(data)
+        del data
+        W=wk(K)
+        W[0,0,0]=0
+        delta_gk_wx=delta_gk*W*1j*(Kx[:,None,None]+zero[None,:,None]+zero[None,None,:])
+        del K
+        result_x=np.array(np.fft.ifftn(delta_gk_wx).real)
+        del delta_gk_wx
+        print 'deltagx is ok'
+############################## delta_g^wy #############################
+        delta_gk_wy=delta_gk*W*1j*(Kx[None,:,None]+zero[:,None,None]+zero[None,None,:])
+        result_y=np.array(np.fft.ifftn(delta_gk_wy).real)
+        del delta_gk_wy
+        print 'deltagy is ok'
+        return (result_x,result_y)
+
+    @classmethod
+    def CalGamma(self,delta_gx,delta_gy):
+        '''cal gammax gammay'''
+        print 'calculating gamma............'
+        gamma1=delta_gx*delta_gx-delta_gy*delta_gy
+        gamma2=2*delta_gx*delta_gy
+        return (gamma1,gamma2)
+
+    @classmethod
+    def CalKappa(self,gamma1,gamma2):
+        print 'calculating kappa_3dx........'
+        gamma1_k=np.fft.fftn(gamma1)
+        gamma2_k=np.fft.fftn(gamma2)
+        nx=np.fft.fftfreq(N,1./N)
+        Kx=2*np.pi/L*nx
+        zero=np.zeros_like(Kx)
+        k1=Kx[:,None,None]+zero[None,:,None]+zero[None,None,:]
+        k2=Kx[None,:,None]+zero[:,None,None]+zero[None,None,:]
+        S=k1**2+k2**2
+
+        S[0,0,:]=np.ones_like(S[0,0,:])
+        kappa_3dk=2*(Kx[:,None,None]**2+Kx[None,:,None]**2+Kx[None,None,:]**2)/(S**2)*((k1**2-k2**2)*gamma1_k+2*k1*k2*gamma2_k)
+        del S
+        kappa_3dx=np.fft.ifftn(kappa_3dk).real
+        print 'calculating kappa_3dx......OK'
+        return kappa_3dx
+        
--- src/result.py
+++ src/result.py
@@ -3,44 +3,30 @@
 import numpy as np
 from TIDES import *
 import h5py
-from mpi4py import MPI
-'''get kappa(kv,kp),b,w, and get kappa(x)'''
-N=1024
-L=1.2*10**3
-#################################################################################
-comm = MPI.COMM_WORLD
-size = comm.Get_size()
-rank = comm.Get_rank()
-#################################################################################
-#f=h5py.File('/home/mtx/data/tide/outdata/0.000den00_wkappa3d_x.hdf5')
-#kappa3dx=np.array(f['data'].value,dtype=np.float16)
-#f.close()
-#################################################################################
-if rank == 0 :
-#    f=h5py.File('/home/mtx/data/tide/outdata/0.000den00_Pk_delta.hdf5')
-#    deltak=np.array(f['data'].value,dtype=np.float16).reshape(size,-1)
-#    f.close()
+import sys
+name=sys.argv[1]
+Inputfilename='/home/zhm/'+name+'/0.000den00.bin'
+Outputfilename='/home/mtx/data/tide/outdata/'+name+'/'
+print Inputfilename
+print Outputfilename
+########################################load data########################################
+delta_x=Tide.LoadData(filename=Inputfilename)
+deltag=Tide.Smooth(data=delta_x,log=True)
+delta_gx,delta_gy=Tide.DeltagW(deltag)
+del deltag
+gamma1,gamma2=Tide.CalGamma(delta_gx,delta_gy)
+del delta_gx
+del delta_gy
+kappa_3dx=Tide.CalKappa(gamma1,gamma2)
+del gamma1
+del gamma2
+#######################################save data#########################################
+Tide.SaveDataHdf5(kappa_3dx,Outputfilename+'0.000den00_kappa3dx.hdf5')
+Pk_delta=Tide.AutoPowerSpectrum(data=delta_x)
+Pk_kappa=Tide.AutoPowerSpectrum(data=kappa_3dx)
+Pk_delta_kappa=Tide.CrossPowerSpectrum(data1=delta_x,data2=kappa_3dx)
 
-    bins=10
-    x=np.fft.fftfreq(N,1./N)
-    z=np.zeros_like(x)
-    KV = ((x**2)[:, None, None] +(x**2)[None, :, None] +(z**2)[None, None, :])**(1. / 2.)
-    KP = ((z**2)[:, None, None] +(z**2)[None, :, None] +(x**2)[None, None, :])**(1. / 2.)
-    kv=kv.reshape(size,-1)
-    kp=kp.reshape(size,-1)
-bin=np.linspace(0,np.log10(512),10,endpoint=False)
-dbin=bin[1]-bin[0]
+Tide.SaveDataHdf5(Pk_kappa,Outputfilename+'0.000den00_Pk_kappa.hdf5')
+Tide.SaveDataHdf5(Pk_delta,Outputfilename+'0.000den00_Pk_delta.hdf5')
+Tide.SaveDataHdf5(Pk_delta_kappa,Outputfilename+'0.000den00_Pk_delta_kappa.hdf5')
 
-a=comm.scatter(deltak,root=0)
-kv=comm.scatter(KV,root=0)
-kp=comm.scatter(KP,root=0)
-bool=[]
-for v in range(10):
-    for p in range(10):
-        if v+p==0:
-            bool[v,p]=
-        bool[i,j]=()
-if rank==0:
-
-
-        
