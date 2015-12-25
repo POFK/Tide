0871136ece4bb99940e9a461d403e3c050b3e880
POFK <maotianxiang@bao.ac.cn>
2015-12-25 21:18:40 +0800

    add some mpi

--- src/halo/get_bin1d.py
+++ src/halo/get_bin1d.py
@@ -15,46 +15,26 @@ comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 ########################## Load data ############################################
-if rank == 0 :
-    f=h5py.File(filename1)#'/home/mtx/data/tide/outdata/old_test/0.000den00_Pk_kappa_delta.hdf5')
-    Pk_d=f['data'].value
-    Pk_d=Pk_d.reshape(8*size,-1)
-    f.close()
-#################################################################################
-    x=np.fft.fftfreq(N,1./N)
-    K = ((x**2)[:, None, None] +(x**2)[None, :, None] +(x**2)[None, None, :])**(1. / 2.)
-    K=K.reshape(8*size,-1)
-else :
-    Pk_d=[]
-    K=[]
+f=h5py.File(filename1,'r')#'/home/mtx/data/tide/outdata/old_test/0.000den00_Pk_kappa_delta.hdf5')
+Pk_d=f['data'][rank*(1024/size):(rank+1)*(1024/size)]
+Pk_d=np.array(Pk_d,dtype=np.float)
+f.close()
+#############################################################################
+x=np.fft.fftfreq(N,1./N)
+K = ((x**2)[rank*(1024/size):(rank+1)*(1024/size)][:, None, None] +(x**2)[None, :, None] +(x**2)[None, None, :])**(1. / 2.)
 ################################################################################
 bins=20
 bin=np.linspace(0,np.log10(512),bins,endpoint=False)
 dbin=bin[1]-bin[0]
-P=[]
-kn=[]
-for i in range(8):
-    P.append(comm.scatter(Pk_d[i::8],root=0))
-    kn.append(comm.scatter(K[i::8],root=0))
-kn=np.array(kn)
-P=np.array(P)
-P=np.array(P,dtype=np.float)
 ################################################################################
-if rank==0:
-    del Pk_d
-    del K
-####################################################################################################
 kx=[]
 Pk=[]
 n=[]
 for i in range(bins):
-    bool=(10**bin[i]<=kn)*(kn<(10**(bin[i]+dbin)))
-    if i==0:
-        if rank==0:
-            bool[0,0]=False
-    n.append(len(kn[bool]))
-    Pk.append(P[bool].sum())
-    kx.append(kn[bool].sum())
+    bool=(10**bin[i]<=K)*(K<(10**(bin[i]+dbin)))
+    n.append(len(K[bool]))
+    Pk.append(Pk_d[bool].sum())
+    kx.append(K[bool].sum())
 n=np.array(n)
 Pk=np.array(Pk)
 kx=np.array(kx)
