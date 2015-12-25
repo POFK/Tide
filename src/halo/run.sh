2c1f62d9f440e40b534a7a20af0bebd90c27d93d
POFK <maotianxiang@bao.ac.cn>
2015-12-25 15:01:22 +0800

    2015/12/25_15:01:21

---
+++ src/halo/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#name='tides00'
+name=$@
+name1='/home/mtx/data/tide/outdata/'$name'/halo/'
+name2='/home/mtx/data/tide/outdata/'$name'/halo/'
+echo $name1'0.000halo00_wfPk_kappa.hdf5' $name2'PS_KK'
+################################################################################
+python result.py $name
+mpirun -hostfile myhostfile python get_bin2d1.py $name
+#python get_wfPk.py $name
+#mpirun -hostfile myhostfile python get_bin1d.py $name1'0.000halo00_wfPk_kappa.hdf5' $name2'PS_KK'
+#mpirun -hostfile myhostfile python get_bin1d.py $name1'0.000halo00_wfPk_delta.hdf5' $name2'PS_DD'
+#mpirun -hostfile myhostfile python get_bin1d.py $name1'0.000halo00_wfPk_delta_kappa.hdf5' $name2'PS_DK'
+
